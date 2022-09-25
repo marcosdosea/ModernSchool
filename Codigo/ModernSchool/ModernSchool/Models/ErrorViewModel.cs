66df712e6d8fdc978de3f0287b428d9e348079e7
matheusmt12 <matheusdacruzsouza1998@gmail.com>
2022-09-25 18:01:20 -0300

    Concluido .#144

---
+++ Codigo/ModernSchool/ModernSchool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModernSchool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
