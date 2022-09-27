d74a8bd696a8ea0ec7b0c8751f4eb19f7e147273
junio Santos <junio.js977@gmail.com>
2022-09-26 23:04:52 -0300

    Foi implementado parte do UC-Manter Período. #154

--- Codigo/ModernSchool/ModernSchool/Program.cs
+++ Codigo/ModernSchool/ModernSchool/Program.cs
@@ -1,4 +1,34 @@
+using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using Service;
+
 var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ModernSchoolContext>(
+    options => options.UseMySQL(builder.Configuration.GetConnectionString("ModernSchoolDatabase")));
+
+builder.Services.AddTransient<IPeriodoService, PeriodoService>();
+//Observacao: só tirar o comentario do que vai implementar
+//builder.Services.AddTransient<IDiarioDeClasseService, DiarioDeClasseService>();
+//builder.Services.AddTransient<IComunicacaoService, ComunicacaoService>();
+//builder.Services.AddTransient<IComponenteService, ComponenteService>();
+//builder.Services.AddTransient<IAutenticarService, AutenticarService>();
+//builder.Services.AddTransient<IPresencaService, PresencaService>();
+//builder.Services.AddTransient<IGradeHorarioService, GradeHorarioService>();
+//builder.Services.AddTransient<IEscolaService, EscolaService>();
+//builder.Services.AddTransient<IPrefeituraService, PrefeituraService>();
+//builder.Services.AddTransient<IServidorService, ServidorService>();
+//builder.Services.AddTransient<ICurriculoService, CurriculoService>();
+//builder.Services.AddTransient<IProfessorService, ProfessorService>();*/
+
+//TODO: Organize essa bagunça
+
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
