e7d818ce6b59e92fa7e7907d92e1adefa30b7e82
John0liver <jhonatas404@outlook.com>
2022-09-29 11:39:59 -0300

    implementado o caso de uso componente #161

---
+++ Codigo/ModernSchool/ModernSchool/Controllers/ComponenteController.cs
@@ -0,0 +1,97 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModernSchool.Models;
+using ModernSchoolWEB.Models;
+using Service;
+
+namespace ModernSchool.Controllers
+{
+    public class ComponenteController : Controller
+    {
+        private readonly IComponenteService _componenteService;
+        private readonly IMapper _mapper;
+
+        public ComponenteController(IComponenteService componenteService, IMapper mapper)
+        {
+            _componenteService = componenteService;
+            _mapper = mapper;
+        }
+
+
+        // GET: ComponenteController
+        public ActionResult Index()
+        {
+            var listaComponente = _componenteService.GetAll();
+            var listaComponenteModel = _mapper.Map<List<ComponenteViewModel>>(listaComponente);
+            return View(listaComponenteModel);
+        }
+
+        // GET: ComponenteController/Details/5
+        public ActionResult Details(int id)
+        {
+            Componente componente = _componenteService.Get(id);
+            ComponenteViewModel componenteModel = _mapper.Map<ComponenteViewModel>(componente);
+            return View(componenteModel);
+        }
+
+        // GET: ComponenteController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ComponenteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ComponenteViewModel componenteViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var componente = _mapper.Map<Componente>(componenteViewModel);
+                _componenteService.Create(componente);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: ComponenteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Componente componente = _componenteService.Get(id);
+            ComponenteViewModel componenteModel = _mapper.Map<ComponenteViewModel>(componente);
+            return View(componenteModel);
+        }
+
+        // POST: ComponenteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ComponenteViewModel componenteViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var componente = _mapper.Map<Componente>(componenteViewModel);
+                _componenteService.Edit(componente);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: ComponenteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Componente componente = _componenteService.Get(id);
+            ComponenteViewModel componenteViewModel = _mapper.Map<ComponenteViewModel>(componente);
+            return View(componenteViewModel);
+        }
+
+        // POST: ComponenteController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, ComponenteViewModel componenteViewModel)
+        {
+            _componenteService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
