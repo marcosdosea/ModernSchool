f5b1bae72921249b025ecbbb351e4f4694d75d82
Reinan <reinanzinho100@gmail.com>
2022-09-29 23:27:49 -0300

    resolvendo o conflito. #167

--- Codigo/ModernSchool/ModernSchool/Models/PeriodoViewModel.cs
+++ Codigo/ModernSchool/ModernSchool/Models/PeriodoViewModel.cs
@@ -3,7 +3,7 @@
     public class PeriodoViewModel
     {
         public int Id { get; set; }
-        public string? Nome { get; set; }
+        public string Nome { get; set; }
         public DateTime DataInicio { get; set; }
         public DateTime DataFim { get; set; }
         public int AnoLetivo { get; set; }
