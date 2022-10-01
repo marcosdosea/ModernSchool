aa6f3c01a9cc242fd290ca1334fd5ef969c649da
junio Santos <junio.js977@gmail.com>
2022-10-01 18:43:12 -0300

    implementado a core, service e a view do UC-Manter Ano Letivo. #179

---
+++ Codigo/ModernSchool/ModernSchool/Mappers/AnoLetivoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core;
+using ModernSchool.Models;
+using ModernSchoolWEB.Models;
+
+namespace ModernSchool.Mappers
+{
+    public class AnoLetivoProfile : Profile
+    {
+        public AnoLetivoProfile()
+        {
+            CreateMap<AnoLetivoViewModel, Anoletivo>().ReverseMap();
+        }
+    }
+}
