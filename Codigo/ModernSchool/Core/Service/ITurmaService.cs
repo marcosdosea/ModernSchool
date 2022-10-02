5728872a070acfe1b332bb68bb1534fcd7d6b740
John0liver <jhonatas404@outlook.com>
2022-10-02 12:04:10 -0300

    Adicionando interface ITurmaService

---
+++ Codigo/ModernSchool/Core/Service/ITurmaService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    internal interface ITurmaService
+    {
+        public interface ITurmaService
+        {
+            int Create(Turma turma);
+            void Edit(Turma turma);
+            void Delete(int id);
+            Turma Get(int id);
+            IEnumerable<Turma> GetAll();
+        }
+    }
+}
