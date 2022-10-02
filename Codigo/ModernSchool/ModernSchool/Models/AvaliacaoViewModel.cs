b6cae0643e5eca56b111ee2b8d5e44712409ce4a
gilmariosantos1 <gilmariosantos1@gmail.com>
2022-10-01 21:08:04 -0300

    Inserindo campos das chaves estranjeiras.

--- Codigo/ModernSchool/ModernSchool/Models/AvaliacaoViewModel.cs
+++ Codigo/ModernSchool/ModernSchool/Models/AvaliacaoViewModel.cs
@@ -9,5 +9,8 @@
         public string TipoDeAtividade { get; set; }
         public int Peso { get; set; }
         public int Avaliativo { get; set; }
+        public int IdTurma { get; set; }
+        public int IdComponente { get; set; }
+        public int IdPeriodo { get; set;}
     }
 }
