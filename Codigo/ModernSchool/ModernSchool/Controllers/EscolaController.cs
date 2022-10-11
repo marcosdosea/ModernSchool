408886f811d05be8dec84c152d9a78ff72da69de
junio Santos <junio.js977@gmail.com>
2022-10-11 15:48:22 -0300

    Foi criado a service, a controller e as view. falta implementa o codigo na service. #165

---
+++ Codigo/ModernSchool/ModernSchool/Controllers/EscolaController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using Core.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModernSchoolWEB.Controllers
+{
+    public class EscolaController : Controller
+    {
+        private readonly IEscolaService _escolaService;
+        private readonly IMapper _mapper;
+        public EscolaController(IEscolaService escolaService, IMapper mapper)
+        {
+            _escolaService = escolaService;
+            _mapper = mapper;
+        }
+        // GET: EscolaController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: EscolaController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EscolaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EscolaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EscolaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EscolaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EscolaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EscolaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Codigo/ModernSchool/ModernSchool/Models/EscolaViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModernSchoolWEB.Models
+{
+    public class EscolaViewModel
+    {
+        [Required(ErrorMessage = "Campo requerido")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(70, ErrorMessage = "No minímo 10 caracteres e no máximo 70.", MinimumLength = 10)]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(14, ErrorMessage = "No minímo 14 caracteres e no máximo 14.", MinimumLength = 14)]
+        public string Cnpj { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public short? Numero { get; set; }
+        [Required(ErrorMessage = "Campo requirido")]
+        public int IdGoverno { get; set; }
+    }
+}
