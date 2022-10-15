7dcd345a7f770dc995ef63b80439965bbeb8894e
junio Santos <junio.js977@gmail.com>
2022-10-15 12:52:40 -0300

    adicionado campos requeridos na ViewModel AnoLetivo e Periodo. #207

--- Codigo/ModernSchool/ModernSchool/Models/AnoLetivoViewModel.cs
+++ Codigo/ModernSchool/ModernSchool/Models/AnoLetivoViewModel.cs
@@ -1,10 +1,19 @@
-﻿namespace ModernSchool.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModernSchool.Models
 {
     public class AnoLetivoViewModel
     {
+        [Key]
+        [Required(ErrorMessage = "Campo requerido")]
         public int AnoLetivo { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
         public int IdEscola { get; set; }
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Campo requerido")]
         public DateTime DataInicio { get; set; }
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Campo requerido")]  
         public DateTime DataFim { get; set; }
     }
 }
--- Codigo/ModernSchool/ModernSchool/Models/PeriodoViewModel.cs
+++ Codigo/ModernSchool/ModernSchool/Models/PeriodoViewModel.cs
@@ -1,11 +1,17 @@
-﻿namespace ModernSchoolWEB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModernSchoolWEB.Models
 {
     public class PeriodoViewModel
     {
+        [Key]
         public int Id { get; set; }
-        public string? Nome { get; set; }
-        public DateTime DataInicio { get; set; }
-        public DateTime DataFim { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(30, ErrorMessage = "O nome do período deve ter no máximo 30 caracteres")]
+        public string Nome { get; set; }
+        public DateTime? DataInicio { get; set; }
+        public DateTime? DataFim { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
         public int AnoLetivo { get; set; }
     }
 }
