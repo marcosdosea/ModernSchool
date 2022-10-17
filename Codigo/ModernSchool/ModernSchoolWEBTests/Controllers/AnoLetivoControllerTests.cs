2001713d1f99c4196c753b1cad9df50165940392
junio Santos <junio.js977@gmail.com>
2022-10-17 16:50:44 -0300

    Adicionado projeto ServiceTests e parte da implementação do teste do AnoLetivo. #214

--- Codigo/ModernSchool/ModernSchoolWEBTests/Controllers/AnoLetivoControllerTests.cs
+++ Codigo/ModernSchool/ModernSchoolWEBTests/Controllers/AnoLetivoControllerTests.cs
@@ -1,7 +1,11 @@
 ﻿using AutoMapper;
+using Core;
 using Core.Service;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ModernSchool.Controllers;
+using ModernSchool.Mappers;
+using ModernSchool.Models;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -22,65 +26,156 @@ namespace ModernSchool.Controllers.Tests
             var mockService = new Mock<IAnoLetivoService>();
 
             IMapper mapper = new MapperConfiguration(cfg =>
-                cfg.AddProfile(new AnoletivoProfile())).CreateMapper();
+                cfg.AddProfile(new AnoLetivoProfile())).CreateMapper();
 
             mockService.Setup(service => service.GetAll())
-                .Returns(GetTestAreasDeServico());
+                .Returns(GetTestAnosLetivos());
             mockService.Setup(service => service.Get(1))
-                .Returns(GetTargetAreaDeServico());
-            mockService.Setup(service => service.Edit(It.IsAny<Areadeservico>()))
+                .Returns(GetTargetAnoLetivo());
+            mockService.Setup(service => service.Edit(It.IsAny<Anoletivo>()))
                 .Verifiable();
-            mockService.Setup(service => service.Create(It.IsAny<Areadeservico>()))
+            mockService.Setup(service => service.Create(It.IsAny<Anoletivo>()))
                 .Verifiable();
-            controller = new AreaDeServicoController(mockService.Object, mapper);
+            controller = new AnoLetivoController(mockService.Object, mapper);
         }
 
         [TestMethod()]
         public void IndexTest()
         {
-            Assert.Fail();
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(List<AnoLetivoViewModel>));
+
+            List<AnoLetivoViewModel> lista = (List<AnoLetivoViewModel>)viewResult.ViewData.Model;
+            Assert.AreEqual(3, lista.Count);
         }
 
         [TestMethod()]
         public void DetailsTest()
         {
-            Assert.Fail();
+            var result = controller.Details(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(AnoLetivoViewModel));
+            AnoLetivoViewModel anoLetivoViewModel = (AnoLetivoViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual(2022, anoLetivoViewModel.AnoLetivo);
+            Assert.AreEqual(1, anoLetivoViewModel.IdEscola);
         }
 
         [TestMethod()]
         public void CreateTest()
         {
-            Assert.Fail();
+            // Act
+            var result = controller.Create();
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
 
         [TestMethod()]
-        public void CreateTest1()
+        public void CreateTest_Post_Valid()
         {
-            Assert.Fail();
+            // Act
+            var result = controller.Create(GetNewAnoLetivo());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        public void CreateTest_Post_InValid()
+        {
+            // Arrange
+            controller.ModelState.AddModelError("Nome", "Campo requerido");
+
+            // Act
+            var result = controller.Create(GetNewAnoLetivo());
+
+            // Assert
+            Assert.AreEqual(1, controller.ModelState.ErrorCount);
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        private AnoLetivoViewModel GetNewAnoLetivo()
+        {
+            throw new NotImplementedException();
         }
 
         [TestMethod()]
-        public void EditTest()
+        public void EditTest_Get()
         {
-            Assert.Fail();
+            var result = controller.Edit(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(AnoLetivoViewModel));
+            AnoLetivoViewModel anoLetivoViewModel = (AnoLetivoViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual(2022, anoLetivoViewModel.AnoLetivo);
+            Assert.AreEqual(1, anoLetivoViewModel.IdEscola);
         }
 
         [TestMethod()]
-        public void EditTest1()
+        public void EditTest_Post()
         {
-            Assert.Fail();
+            // Act
+            var result = controller.Edit(GetTargetAnoLetivoViewModel().Id, GetTargetAnoLetivoViewModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        private object GetTargetAnoLetivoViewModel()
+        {
+            throw new NotImplementedException();
         }
 
         [TestMethod()]
-        public void DeleteTest()
+        public void DeleteTest_Get()
         {
-            Assert.Fail();
+            var result = controller.Delete(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(AnoLetivoViewModel));
+            AnoLetivoViewModel anoLetivoViewModel = (AnoLetivoViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual(2022, anoLetivoViewModel.AnoLetivo);
+            Assert.AreEqual(1, anoLetivoViewModel.IdEscola);
         }
 
         [TestMethod()]
-        public void DeleteTest1()
+        public void DeleteTest_Post()
         {
-            Assert.Fail();
+            // Act
+            var result = controller.Delete(GetTargetAnoLetivoViewModel().Id, GetTargetAnoLetivoViewModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+        private Anoletivo GetTargetAnoLetivo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<Anoletivo> GetTestAnosLetivos()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Codigo/ModernSchool/ServiceTests/AnoLetivoServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Tests
+{
+    [TestClass()]
+    public class AnoLetivoServiceTests
+    {
+        [TestMethod()]
+        public void AnoLetivoServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
