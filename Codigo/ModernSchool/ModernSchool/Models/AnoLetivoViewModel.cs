00e2738c80ff743f076769086199fab3b0380656
junio Santos <junio.js977@gmail.com>
2022-10-17 15:06:00 -0300

    Adicionado validações em AnoLetivoViewModel. #213

--- Codigo/ModernSchool/ModernSchool/Models/AnoLetivoViewModel.cs
+++ Codigo/ModernSchool/ModernSchool/Models/AnoLetivoViewModel.cs
@@ -5,15 +5,19 @@ namespace ModernSchool.Models
     public class AnoLetivoViewModel
     {
         [Key]
+        [Display(Name = "Ano Letivo")]
         [Required(ErrorMessage = "Campo requerido")]
         public int AnoLetivo { get; set; }
         [Required(ErrorMessage = "Campo requerido")]
+        [Display(Name = "Escola")]
         public int IdEscola { get; set; }
-        [DataType(DataType.Date)]
-        [Required(ErrorMessage = "Campo requerido")]
+        [Display(Name = "Data de início")]
+        [DataType(DataType.Date, ErrorMessage = "Data válida requerida")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataInicio { get; set; }
-        [DataType(DataType.Date)]
-        [Required(ErrorMessage = "Campo requerido")]  
+        [Display(Name = "Data de fim")]
+        [DataType(DataType.Date, ErrorMessage = "Data válida requerida")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataFim { get; set; }
     }
 }
