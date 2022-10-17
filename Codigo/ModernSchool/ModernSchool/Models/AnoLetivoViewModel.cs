83a763a48ab85c47e2703c605bc2e4ca79d4fc1f
junio Santos <junio.js977@gmail.com>
2022-10-17 15:17:34 -0300

    Validações adicionadas no PeriodoViewModel. #213

--- Codigo/ModernSchool/ModernSchool/Models/AnoLetivoViewModel.cs
+++ Codigo/ModernSchool/ModernSchool/Models/AnoLetivoViewModel.cs
@@ -6,18 +6,18 @@ namespace ModernSchool.Models
     {
         [Key]
         [Display(Name = "Ano Letivo")]
-        [Required(ErrorMessage = "Campo requerido")]
-        public int AnoLetivo { get; set; }
-        [Required(ErrorMessage = "Campo requerido")]
+        [Required(ErrorMessage = "Campo ano letivo é obrigatório")]
+        public int? AnoLetivo { get; set; }
+        [Required(ErrorMessage = "Campo escola é obrigatório")]
         [Display(Name = "Escola")]
-        public int IdEscola { get; set; }
+        public int? IdEscola { get; set; }
         [Display(Name = "Data de início")]
         [DataType(DataType.Date, ErrorMessage = "Data válida requerida")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-        public DateTime DataInicio { get; set; }
+        public DateTime? DataInicio { get; set; }
         [Display(Name = "Data de fim")]
         [DataType(DataType.Date, ErrorMessage = "Data válida requerida")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-        public DateTime DataFim { get; set; }
+        public DateTime? DataFim { get; set; }
     }
 }
