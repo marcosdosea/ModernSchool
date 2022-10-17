b39e82b8323459570e907fe8f15bdf88175ae2cd
John0liver <jhonatas404@outlook.com>
2022-10-17 08:13:00 -0300

    Adicionando interface ITurmaService

---
+++ Codigo/ModernSchool/Core/Service/IUnidadeTematica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IUnidadeTematica
+    {
+        int Create(Unidadetematica unidadetematica);
+        void Edit(Unidadetematica unidadetematica);
+        void Delete(int id);
+        Turma Get(int id);
+        IEnumerable<Unidadetematica> GetAll();
+    }
+}
