aaaef20bd2ad3a2b4392430609cb535984fb555c
Reinan <reinanzinho100@gmail.com>
2022-10-19 18:23:25 -0300

    Subindo DTO. resolve #234

---
+++ Codigo/ModernSchool/ModernSchool/Mappers/EscolaProfessorDTOProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core.DTO;
+using ModernSchoolWEB.Models;
+
+namespace ModernSchoolWEB.Mappers
+{
+    public class EscolaProfessorDTOProfile : Profile
+    {
+        public EscolaProfessorDTOProfile()
+        {
+            CreateMap<EscolaProfessorDTOViewModel, EscolaProfessorDTO>().ReverseMap();
+        }
+    }
+}
