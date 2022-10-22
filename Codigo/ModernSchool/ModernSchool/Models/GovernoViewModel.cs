99d3e54c84a79295775b3a22bf0541c982f7b70c
matheusmt12 <matheusdacruzsouza1998@gmail.com>
2022-10-22 15:44:21 -0300

    Alteração concluida #248

--- Codigo/ModernSchool/ModernSchool/Models/GovernoViewModel.cs
+++ Codigo/ModernSchool/ModernSchool/Models/GovernoViewModel.cs
@@ -4,13 +4,23 @@ namespace ModernSchoolWEB.Models
 {
     public class GovernoViewModel
     {
-        [Required]
+        [Key]
+        [Display(Name = "Codigo")]
         public int Id { get; set; }
-        [Required]
-        public string? Municipio { get; set; }
-        [Required]
+        
+        [Display(Name = "Municipio")]
+        [Required(ErrorMessage ="O nome do municipio é necessário")]
+        [StringLength(30,MinimumLength = 5, ErrorMessage = "A quantidade de caracteres não é aceitavel")]
+        public string Municipio { get; set; }
+        
+        [Display (Name = "Estado")]
+        [Required(ErrorMessage =" Inserir as siglas do Estado")]
+        [StringLength(2,MinimumLength =2 , ErrorMessage = "É necessário dois caracteres")]
         public string? Estado { get; set; }
-        [Required]
+        
+        [Display (Name = "Dependencia Administrativa")]
+        [Required(ErrorMessage ="Campo necessario ")]
+        [StringLength(1,MinimumLength = 1)]
         public string? DependenciaAdministrativa { get; set; }
     }
 }
