93dd45e102890f4ff6cfca6ce6a7bdbba53909ba
Moisés Santos <50469065+junio10@users.noreply.github.com>
2022-10-25 15:16:05 -0300

    Update GovernoViewModel.cs
    atualizando erros de espaçamento.

--- Codigo/ModernSchool/ModernSchool/Models/GovernoViewModel.cs
+++ Codigo/ModernSchool/ModernSchool/Models/GovernoViewModel.cs
@@ -9,17 +9,17 @@ namespace ModernSchoolWEB.Models
         public int Id { get; set; }
         
         [Display(Name = "Municipio")]
-        [Required(ErrorMessage ="O  campo município é obrigatório")]
+        [Required(ErrorMessage ="O campo município é obrigatório")]
         [StringLength(30,MinimumLength = 5, ErrorMessage = "No minímo 5 caracteres e no máximo 30.")]
         public string Municipio { get; set; }
         
         [Display (Name = "Estado")]
-        [Required(ErrorMessage ="O  campo estado é obrigatório")]
+        [Required(ErrorMessage ="O campo estado é obrigatório")]
         [StringLength(2,MinimumLength =2 , ErrorMessage = "É necessário dois caracteres")]
         public string? Estado { get; set; }
         
         [Display (Name = "Dependencia Administrativa")]
-        [Required(ErrorMessage ="O  campo Dependencia Administrativa é obrigatório")]
+        [Required(ErrorMessage ="O campo Dependencia Administrativa é obrigatório")]
         [StringLength(1,MinimumLength = 1)]
         public string? DependenciaAdministrativa { get; set; }
     }
