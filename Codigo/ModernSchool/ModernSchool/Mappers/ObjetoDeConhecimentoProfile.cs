b624370ddd7b46b2015da67f44ce54be862b22e1
John0liver <jhonatas404@outlook.com>
2022-10-25 14:50:26 -0300

    Implementado ObjetoDeConhecimentoProfile

---
+++ Codigo/ModernSchool/ModernSchool/Mappers/ObjetoDeConhecimentoProfile.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+using AutoMapper;
+using ModernSchoolWEB.Models;
+
+namespace ModernSchoolWEB.Mappers
+{
+    public class ObjetoDeConhecimentoProfile : Profile
+    {
+        public ObjetoDeConhecimentoProfile()
+        {
+            CreateMap<ObjetoDeConhecimentoViewModel, Objetodeconhecimento>().ReverseMap();
+        }
+    }
+}
