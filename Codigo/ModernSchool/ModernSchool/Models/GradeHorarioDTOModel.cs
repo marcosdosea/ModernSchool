cefee982a4d309a740941932b75d914c16902b87
matheusmt12 <matheusdacruzsouza1998@gmail.com>
2022-11-23 21:34:19 -0300

    Model melhorado #247

--- Codigo/ModernSchool/ModernSchool/Models/GradeHorarioDTOModel.cs
+++ Codigo/ModernSchool/ModernSchool/Models/GradeHorarioDTOModel.cs
@@ -9,12 +9,16 @@ namespace ModernSchoolWEB.Models
 
         [Display(Name ="Componente")]
         public string? Componente { get; set; }
+        [Required(ErrorMessage = "Este campo é requerido")]
         [Display(Name ="Dia")]
         public string? Dia { get; set; }
+        [Required(ErrorMessage ="Este campo é requerido")]
         [Display(Name ="Hora inicio")]
         public string? HoraInicio { get; set; }
-        [Display(Name ="HoraFim fim")]
+        [Required(ErrorMessage = "Este campo é requerido")]
+        [Display(Name ="Hora Fim")]
         public string? HoraFim { get; set; }
+        [Required(ErrorMessage = "Este campo é requerido")]
         [Display(Name ="Professor")]
         public string? Professor { get; set; }
     }
--- Codigo/ModernSchool/ModernSchool/Models/GradehorarioViewModel.cs
+++ Codigo/ModernSchool/ModernSchool/Models/GradehorarioViewModel.cs
@@ -8,16 +8,23 @@ namespace ModernSchoolWEB.Models
         [Required]
         public int Id { get; set; }
         [Required]
+        [Display(Name ="Componente")]
         public int IdComponente { get; set; }
         [Required]
+        [Display(Name = "Turma")]
         public int IdTurma { get; set; }
-        [Required]
+      
+        [Required (ErrorMessage ="Este campo é requerido")]
+        [Display(Name = "Dia da Semana")]
         public string? DiaSemana { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Este campo é requerido")]
+        [Display(Name = "Hora Inicio")]
         public string? HoraInicio { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Este campo é requerido")]
+        [Display(Name = "Hora Fim")]
         public string? HoraFim { get; set; }
         [Required]
+        [Display(Name = "Professor")]
         public int IdProfessor { get; set; }
 
         public SelectList? ListaComponentes { get; set; }
