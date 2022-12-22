4813154ad454ad6b79f098ae1fa11f3fccfa0b7f
junio10 <junio.js977@gmail.com>
2022-12-22 18:51:16 -0300

    Criado o AlunoAvaliacaoViewModel e AlunoAvaliacaoController. #287

---
+++ Codigo/ModernSchool/ModernSchool/Controllers/AlunoAvaliacaoController.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModernSchoolWEB.Controllers
+{
+    public class AlunoAvaliacaoController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IAvaliacaoService _avaliacaoService;
+        private readonly IAlunoAvaliacaoService _alunoAvaliacaoService;
+        private readonly IPessoaService _pessoaService;
+        // GET: AlunoAvaliacaoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AlunoAvaliacaoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AlunoAvaliacaoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AlunoAvaliacaoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AlunoAvaliacaoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AlunoAvaliacaoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AlunoAvaliacaoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AlunoAvaliacaoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Codigo/ModernSchool/ModernSchool/Models/AlunoAvaliacaoDTOModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ModernSchoolWEB.Models
+{
+    public class AlunoAvaliacaoDTOModel
+    {
+        [Display(Name = "Id Aluno")]
+        public int IdAluno { get; set; }
+        [Display(Name = "Id Avaliacao")]
+        public int IdAvaliacao { get; set; }
+        public decimal Nota { get; set; }
+        [Display(Name = "Data da entrega")]
+        public DateTime DataEntrega { get; set; }
+        [Display(Name = "Material de envio")]
+        public byte[] Arquivo { get; set; }
+        [Display(Name = "Nome Aluno")]
+        public string NomeAluno { get; set; }
+        [Display(Name = "Nome Turma")]
+        public string NomeTurma { get; set; }
+        [Display(Name = "Avaliativo")]
+        public bool isAvaliativo { get; set; }
+    }
+}
