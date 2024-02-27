dd01448fdabee675737d544f6283c57ec41ea3a0
matheus <matheusdacruzsouza1998@gmail.com>
2024-02-27 16:03:32 -0300

    Criando avaliacao com o professor logado

---
+++ Codigo/ModernSchool/Core/DTO/AvaliacaoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class AvaliacaoDTO
+    {
+        public int Id { get; set; }
+        public DateTime DataEntrega { get; set; }
+        public string TipoDeAtividade { get; set; } =string.Empty;
+        public short Peso { get; set; }
+    }
+}
---
+++ Codigo/ModernSchool/ModernSchool/Models/AvaliacaoProfessorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Core.DTO;
+
+namespace ModernSchoolWEB.Models
+{
+    public class AvaliacaoProfessorViewModel
+    {
+        public List<AvaliacaoDTO> Avalicoes { get; set; }
+        public int IdTurma {  get; set; }
+        public int IdComponente { get; set; }
+
+
+    }
+}
