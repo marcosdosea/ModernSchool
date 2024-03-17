6bf00bbacb531d4a319cfc5dac05c99612d86b5f
Reinan de Jesus Santos <reinan.stsdev@gmail.com>
2024-03-17 11:42:36 -0300

    Subindo dados que tinha sumidos e adicionando mascara e corrigindo o matricular aluno

---
+++ Codigo/ModernSchool/ModernSchool/wwwroot/js/Mascara.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $("#cpf").inputmask("999.999.999-99"); //static mask
+    $("#cep").inputmask("99999-999");
+    $('#nome').on('input', function () {
+        var texto = $(this).val();
+        var textoCapitalizado = capitalizarPrimeirasLetras(texto);
+        $(this).val(textoCapitalizado);
+    });
+    function limparCampo(idCampo) {
+        $(idCampo).inputmask('remove');
+        $(idCampo).val("");
+    }
+});
+function capitalizarPrimeirasLetras(texto) {
+    return texto.toLowerCase().replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
+}
--- Codigo/ModernSchool/ModernSchool/wwwroot/js/site.js
+++ Codigo/ModernSchool/ModernSchool/wwwroot/js/site.js
@@ -2,3 +2,36 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#cpf').on('change', function () {
+        var cpf = $(this).val();
+        $.ajax({
+            url: 'BuscarAlunoPorCPF',
+            type: 'POST',
+            data: { cpf: cpf },
+            success: function (aluno) {
+                if (aluno) {
+                    $('#nome').val(aluno.nome = capitalizarPrimeirasLetras(aluno.nome));
+                    $('#dataNascimento').val(aluno.dataNascimento.substring(0, 10));
+                    $('#cep').val(aluno.cep);
+                    $('#rua').val(aluno.rua);
+                    $('#bairro').val(aluno.bairro);
+                    $('#numero').val(aluno.numero);
+                    $('#email').val(aluno.email);
+                } else {
+                    // Limpar os campos do formulário
+                    $('#nome').val('');
+                    $('#dataNascimento').val('');
+                    $('#cep').val('');
+                    $('#rua').val('');
+                    $('#bairro').val('');
+                    $('#numero').val('');
+                    $('#email').val('');
+                }
+            },
+        });
+    });
+    function capitalizarPrimeirasLetras(texto) {
+        return texto.toLowerCase().replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
+    }
+});
