97d7e0f0fe9d972833d17b1f9d5a944422a765ae
matheusmt12 <82002419+matheusmt12@users.noreply.github.com>
2024-04-03 16:20:59 -0300

    Revert "Adição de modal e das notificações para matricular aluno"

--- Codigo/ModernSchool/ModernSchool/Models/PessoaViewModel.cs
+++ Codigo/ModernSchool/ModernSchool/Models/PessoaViewModel.cs
@@ -33,7 +33,7 @@ namespace ModernSchoolWEB.Models
         [Display(Prompt = "Ex: Nome da Cidade")]
         public string Cidade { get; set; } = string.Empty;
         [Display(Prompt = "Ex: Ponto de referência")]
-        public string? Complemento { get; set; }
+        public string Complemento { get; set; } = string.Empty;
         [Display(Name = "Telefone 1", Prompt = "Ex: (99)9 9999-9999")]
         [Required(ErrorMessage = "Campo Telefone 1 é Obrigatório")]
         public string Telefone1 { get; set; } = string.Empty;
