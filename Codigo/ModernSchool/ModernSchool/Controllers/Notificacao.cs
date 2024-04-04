8892141825e285afe6f737b0f8193aa71db7072b
Reinan de Jesus Santos <reinan.stsdev@gmail.com>
2024-04-04 07:50:34 -0300

    subindo alyout notificação e adicionando nome do aluno nas notificação

--- Codigo/ModernSchool/ModernSchool/Controllers/Notificacao.cs
+++ Codigo/ModernSchool/ModernSchool/Controllers/Notificacao.cs
@@ -19,18 +19,22 @@ namespace ModernSchoolWEB.Controllers
                 case Notifica.Sucesso:
                     TempData["alertType"] = "success";
                     TempData["alertIcon"] = "fa-solid fa-circle-check";
+                    TempData["corClose"] = "text-success";
                     break;
                 case Notifica.Erro:
                     TempData["alertType"] = "danger";
                     TempData["alertIcon"] = "fa-solid fa-circle-xmark";
+                    TempData["corClose"] = "text-danger";
                     break;
                 case Notifica.Informativo:
                     TempData["alertType"] = "info";
                     TempData["alertIcon"] = "fa-solid fa-circle-info";
+                    TempData["corClose"] = "text-info";
                     break;
                 case Notifica.Alerta:
                     TempData["alertType"] = "warning";
                     TempData["alertIcon"] = "fa-solid fa-circle-exclamation";
+                    TempData["corClose"] = "text-warning";
                     break;
             }
             TempData["alertText"] = mensagem;
