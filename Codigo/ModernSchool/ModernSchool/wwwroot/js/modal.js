484b1be88f4aaee58f2c946be2e786add139921d
Reinan de Jesus Santos <reinan.stsdev@gmail.com>
2024-04-11 11:00:08 -0300

    Adição de moal

--- Codigo/ModernSchool/ModernSchool/wwwroot/js/modal.js
+++ Codigo/ModernSchool/ModernSchool/wwwroot/js/modal.js
@@ -23,11 +23,18 @@ function exibirMensagemNoModal(mensagem) {
 function showConfirmationModal(formId, view, idModal, nome) {
     var modal = document.getElementById(idModal);
     var modalForm = modal.querySelector('form');
+    var artigo;
+    if (view == 'Turma') {
+        artigo = 'a';
+    } else {
+        artigo = 'o';
+    }
+    view = removerAcentos(view);
     modalForm.action = '/' + view + '/Delete/' + formId; // Define a ação do formulário com a rota correta
     var modalBody = modal.querySelector('.modal-body');
     var modalH5 = modal.querySelector('h5');
     modalH5.innerHTML = 'Confirmar Exclusão do ' + view;
-    modalBody.innerHTML = 'Deseja <b>Excluir</b> o ' + view + ' <b>' + nome + '</b>?'; // Conteúdo do modal
+    modalBody.innerHTML = 'Deseja <b>Excluir</b> '+ artigo + ' ' + view + ' <b>' + nome + '</b>?'; // Conteúdo do modal
 
     // Exibe o modal
     var modal = new bootstrap.Modal(modal);
@@ -52,6 +59,24 @@ function showModalCancelarAluno(idAluno, idTurma) {
     modalForm.action = '/Pessoa/Delete/' + idAluno + '?idTurma=' + idTurma; // Define a ação do formulário com a rota correta
     var modalBody = modal.querySelector('.modal-body');
     modalBody.innerHTML = 'Deseja <b>Cancelar</b> o Aluno(a) <b> </b>?'; // Conteúdo do modal
+    // Exibe o modal
+    var modal = new bootstrap.Modal(modal);
+    modal.show();
+}
+function removerAcentos(texto) {
+    return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+function showConfirmationModalDiario(idObjeto, idDiario, idTabela, uniTematica) {
+    var modal = document.getElementById(idTabela);
+    var modalForm = modal.querySelector('form');
+    view = removerAcentos(view);
+    modalForm.action = '/Professor/DeleteDiarioClasse /?IdDiario=' + idDiario + '&IdObjeto=' + idObjeto +; // Define a ação do formulário com a rota correta
+    var modalBody = modal.querySelector('.modal-body');
+    var modalH5 = modal.querySelector('h5');
+    modalH5.innerHTML = 'Confirmar Exclusão do ' + view;
+    modalBody.innerHTML = 'Deseja <b>Excluir</b> o Diário de Classe da unidade Temática <b>: ' + uniTematica + '</b>?'; // Conteúdo do modal
+
     // Exibe o modal
     var modal = new bootstrap.Modal(modal);
     modal.show();
